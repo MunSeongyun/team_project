7f873e3e1f723489d5ec48eba76f4bb4bc886270
NANASHISHIROUTO <gudtjs1004sd@naver.com>
2024-06-12 22:24:45 +0900

    CRUD, CSS

--- src/pages/member/components/HomeTimeLine.jsx
+++ src/pages/member/components/HomeTimeLine.jsx
@@ -5,6 +5,7 @@ import { Link } from 'react-router-dom';
 
 // Home에서 받아옴
 const HomeTimeLine = ({ id, title, author, image }) => {
+  console.log(image);
   return (
     <div>
       <div className={styles.card}>
