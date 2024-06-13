dba5a936dbf5b3f6ca0f2e297bdd26862fc15476
munseongyun <borygashill608@gmail.com>
2024-06-13 12:02:02 +0900

    테스트 파일 삭제

--- src/index.js
+++ src/index.js
@@ -1,14 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import ImgTest from './ImgTest.js'
-import ImgPrint from './ImgPrint.js'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  // <><ImgTest></ImgTest>
-  // <ImgPrint></ImgPrint>
-  // </>
-  
-  <App />
+    <App />
 );
