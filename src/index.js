ec008b07c1c81c39b3e6b8b7515d09698b3c6d3b
munseongyun <borygashill608@gmail.com>
2024-06-13 11:34:37 +0900

    이미지 추가 테스트

--- src/index.js
+++ src/index.js
@@ -1,8 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import ImgTest from './ImgTest.js'
+import ImgPrint from './ImgPrint.js'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <App />
+  <><ImgTest></ImgTest>
+  <ImgPrint></ImgPrint>
+  </>
+  
+  //<App />
 );
