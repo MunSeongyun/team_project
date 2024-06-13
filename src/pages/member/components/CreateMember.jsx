4da772ce3fc4da5ba2326de0dac861481c6d81f2
munseongyun <borygashill608@gmail.com>
2024-06-13 12:01:24 +0900

    조원소개 create 이미지 base64인코딩 적용

--- src/pages/member/components/CreateMember.jsx
+++ src/pages/member/components/CreateMember.jsx
@@ -10,32 +10,60 @@ const CreateMember = () => {
     const [member, setMember] = useState({
         author: "",
         title: "",
-        description: "",
-        image: "",
-        descriptionImage: ""
+        description: ""
     })
 
     const onChange = (e) => {
         const { target: { name, value } } = e
         setMember({ ...member, [name]: value })
     }
+    
+    const [image, setImage] = useState(null);
+
+    const imgChange = (e) => {
+        const file = e.target.files[0];
+        const reader = new FileReader();
+        console.log(reader);
+        reader.onload = () => {
+            const imgbase64 = reader.result;
+            setImage(imgbase64);
+        };
+
+        reader.readAsDataURL(file);
+    };
+    const [descriptionImage, setDescriptionImage] = useState(null);
+
+    const descriptionImageChange = (e) => {
+        const file1 = e.target.files[0];
+        const reader1 = new FileReader();
+
+        reader1.onload = () => {
+            const desbase64 = reader1.result;
+            setDescriptionImage(desbase64);
+        };
+
+        reader1.readAsDataURL(file1);
+    };
 
     const onSubmit = (e) => {
         e.preventDefault()
+        const post = {
+            ...member,
+            image:image,
+            descriptionImage:descriptionImage
+        }
         fetch(`http://localhost:5000/member_posts`, {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json'
             },
-            body: JSON.stringify(member)
+            body: JSON.stringify(post)
         })
             .then((response) => response.json())
             .then((response) => setMember({
                 author: "",
                 title: "",
-                description: "",
-                image: "",
-                descriptionImage: ""
+                description: ""
             }))
             .then(()=>navigate('/member'))
     }
@@ -44,25 +72,25 @@ const CreateMember = () => {
         <>
         <Figure />
             <form onSubmit={onSubmit} method='POST'>
-                <div class="form-group">
-                    <label for="exampleInputEmail1">이름</label>
+                <div className="form-group">
+                    <label htmlFor="exampleInputEmail1">이름</label>
                     <input onChange={onChange} value={member.author} name='author' className="form-control" placeholder="Enter email" />
                 </div>
                 <div className="form-group">
-                    <label for="exampleInputPassword1">좌우명</label>
+                    <label htmlFor="exampleInputPassword1">좌우명</label>
                     <input onChange={onChange} value={member.title} name='title' className="form-control" placeholder="Password" />
                 </div>
                 <div className="form-group">
-                    <label for="exampleFormControlTextarea1">본문</label>
+                    <label htmlFor="exampleFormControlTextarea1">본문</label>
                     <textarea onChange={onChange} value={member.description} name='description' className="form-control" rows="3"></textarea>
                 </div>
-                <div class="form-group">
-                    <label for="exampleInputEmail1">메인이미지</label>
-                    <input onChange={onChange} value={member.image} name='image' className="form-control" placeholder="Enter email" />
+                <div className="form-group">
+                    <label htmlFor="exampleInputEmail1">메인이미지</label>
+                    <input type='file' onChange={imgChange} name='image' className="form-control" placeholder="Enter email" />
                 </div>
-                <div class="form-group">
-                    <label for="exampleInputEmail1">본문이미지</label>
-                    <input onChange={onChange} value={member.descriptionImage} name='descriptionImage' className="form-control" placeholder="Enter email" />
+                <div className="form-group">
+                    <label htmlFor="exampleInputEmail1">본문이미지</label>
+                    <input type='file' onChange={descriptionImageChange} name='descriptionImage' className="form-control" placeholder="Enter email" />
                 </div>
                 <button className="btn btn-primary">Submit</button>
             </form>
